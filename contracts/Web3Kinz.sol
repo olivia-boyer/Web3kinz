c1ba1eff05b9b26b44f1b4ead2b5c274d0f6e030
Glory Zhang <glorytanzhang@gmail.com>
2026-03-31 17:06:56 -0400

    bare bones

---
+++ contracts/Web3Kinz.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+contract Web3Kinz {
+    // insert variables here
+    
+    // constructor
+    constructor() payable {
+        //
+    }
+
+    // modifier for only owner
+
+    // insert functions here
+    function functionName() public {
+        //
+    }
+}
